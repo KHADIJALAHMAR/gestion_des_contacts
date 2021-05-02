20ceea933b6a583d946b2e81a544880ebf6e6bc0
asmaatalha <asmaa.talha1998@gmail.Com>
2021-05-02 21:07:58 +0000

    modification de profile

--- app/views/users/profile.php
+++ app/views/users/profile.php
@@ -2,25 +2,21 @@
 <?php require APPROOT .'/views/inc/header.php';?>
 <!-- end header -->
     
-    <h1>Welcome, <?php echo $data['username'] ?>!</h1>
+    <h1 class="my-sm-5">Welcome, <?php echo $data['username'] ?>!</h1>
 
-    <h3>Your profile:</h3>
+    <h3 class="my-sm-4">Your profile:</h3>
     <hr>
 
-    <label>Username:</label>
-    <span><?php echo $data['username']; ?></span>
+    <label class="col-lg-1 font-weight-bold">Username:</label>
+    <span class="col-lg-6"><?php echo $data['username']; ?></span>
     <hr>
 
-    <label>Signup date:</label>
-    <span><?php echo $data['created_at']; ?></span>
+    <label class="col-lg-1 font-weight-bold">Signup date:</label>
+    <span class="col-lg-2"><?php $day = date('l,d F'); echo $day . ' ' . $data['created_at']; ?></span>
     <hr>
 
-    <label>Last login:</label>
-    <span><?php echo $data['created_at']; ?></span>
-
-
-
-
+    <label class="col-lg-1 font-weight-bold">Last login:</label>
+    <span class="col-lg-2"><?php $day1 = date('l,d F Y'); echo $day1 . ' ' . $data['last_login']; ?></span>
 
 <!-- footer -->
 <?php require APPROOT .'/views/inc/footer.php'; ?>
